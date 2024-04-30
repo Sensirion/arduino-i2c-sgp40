acc029b03fc79ff8cc0fec39968c76af48672e3d
LeonieFierz <56353030+LeonieFierz@users.noreply.github.com>
2024-04-30 10:11:59 +0200

    Fix comment for i2cBus parameter

--- src/SensirionI2CSgp40.h
+++ src/SensirionI2CSgp40.h
@@ -50,7 +50,7 @@ class SensirionI2CSgp40 {
     /**
      * begin() - Initializes the SensirionI2CSgp40 class.
      *
-     * @param serial Arduino stream object to be communicated with.
+     * @param i2cBus Arduino stream object to use for communication.
      *
      */
     void begin(TwoWire& i2cBus);
